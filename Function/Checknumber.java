ae09138a4249dc3d4b0fbe0fc75f2a4571abfed4
KunalKoushik <kunalkoushik44@gmail.com>
2023-10-20 18:58:17 +0530

    Function

---
+++ Function/Checknumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Checknumber {
+    static void check(int num){
+
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+    }
+}
